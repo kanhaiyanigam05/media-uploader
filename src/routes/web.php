67707eba197e9b047d64b12df74336808870315f
kanhaiyanigam05 <148037979+kanhaiyanigam05@users.noreply.github.com>
2024-12-10 17:04:52 +0530

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,22 @@
 use Media\Uploader\Http\Controllers\MediaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('media');
-});
 Route::post('upload/url', [MediaController::class, 'uploadUrl'])->name('media.upload.url');
+Route::post('media-item', [MediaController::class, 'mediaItem'])->name('media.item');
 Route::resource('media', MediaController::class);
+
+
+Route::post('media-item', function (HttpRequest $request) {
+    $media = (object) $request->input('media');
+    if (isset($media->created_at)) {
+        $media->created_at = Carbon::parse($media->created_at);
+    }
+    return response()->json([
+        'html' => view('components.media-item', compact('media'))->render(),
+    ]);
+})->name('media.item');
+Route::post('input', function (Request $request) {
+    return response()->json([
+        'html' => view('components.input', $request->all())->render(),
+    ]);
+})->name('input');
